e504df44b1f4646ea024b0b0c6c3fb04b98fa2f4
Jerome Covington <jeromecovington@gmail.com>
2018-05-11 19:46:29 -0400

    Added ability example

---
+++ examples/ability.js
@@ -0,0 +1,31 @@
+const { BaseCreature, BaseRoll } = require('../index')
+const { roll } = require('../src/roll/helper')
+
+const abilityMap = require('../test/exampleFeats/abilityMap')
+const difficultyMap = require('../test/exampleFeats/difficultyMap')
+
+const Roll = new BaseRoll({
+  abilityMap,
+  difficultyMap
+})
+
+const Theif = new BaseCreature({
+  strength: 8,
+  dexterity: 16,
+  constitution: 10,
+  intelligence: 12,
+  wisdom: 8,
+  charisma: 12,
+  advantages: ['dexterity'],
+  disadvantages: ['strength'],
+  hitPointsRollFunc: () => roll(1, 10)
+})
+Theif.setHitPoints(0)
+
+console.log('The theif attempts to sneak past the sleeping giant.')
+const result = Roll.abilityCheck(Theif, 'stealth', 'medium')
+if (result) {
+  console.log('The theif enters the giant\'s treasure room undetected.')
+} else {
+  console.log('The giant awakens, and roars angrily.')
+}
