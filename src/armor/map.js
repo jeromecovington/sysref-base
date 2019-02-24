1c9825d3fcd5dfd5314573ccb1c8ef69c199342e
Jerome T.K. Covington <jeromecovington@gmail.com>
2019-02-24 12:30:20 -0500

    fill out armor map

--- src/armor/map.js
+++ src/armor/map.js
@@ -1,1 +1,68 @@
 // https://5thsrd.org/adventuring/equipment/armor/
+module.exports = {
+  padded: {
+    cost: 5,
+    modifier: 11,
+    usesDexterity: true
+  },
+  leather: {
+    cost: 10,
+    modifier: 11,
+    usesDexterity: true
+  },
+  studdedLeather: {
+    cost: 45,
+    modifier: 12,
+    usesDexterity: true
+  },
+  hide: {
+    cost: 10,
+    modifier: 12,
+    usesDexterity: true,
+    maxDexterity: 2
+  },
+  chainSkirt: {
+    cost: 50,
+    modifier: 13,
+    usesDexterity: true,
+    maxDexterity: 2
+  },
+  scaleMail: {
+    cost: 50,
+    modifier: 14,
+    usesDexterity: true,
+    maxDexterity: 2
+  },
+  breastPlate: {
+    cost: 400,
+    modifier: 14,
+    usesDexterity: true,
+    maxDexterity: 2
+  },
+  halfPlate: {
+    cost: 750,
+    modifier: 15,
+    usesDexterity: true,
+    maxDexterity: 2
+  },
+  ringMail: {
+    cost: 30,
+    modifier: 14
+  },
+  chainMail: {
+    cost: 75,
+    modifier: 16
+  },
+  splint: {
+    cost: 200,
+    modifier: 17
+  },
+  plate: {
+    cost: 1500,
+    modifier: 18
+  },
+  shield: {
+    cost: 10,
+    modifier: 2
+  }
+};
