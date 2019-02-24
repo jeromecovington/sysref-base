b44f02c496ee3378f47e2d25c55b2a5802dc9e8a
Jerome T.K. Covington <jeromecovington@gmail.com>
2019-02-24 13:44:43 -0500

    add weapons map

---
+++ src/weapons/map.js
@@ -0,0 +1,165 @@
+// https://5thsrd.org/adventuring/equipment/weapons/
+
+module.exports = {
+  club: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 1,
+    minMax: [1, 4]
+  },
+  dagger: {
+    abilities: ['dexterity', 'strength'],
+    classification: ['melee', 'ranged'],
+    cost: 2,
+    minMax: [1, 4]
+  },
+  greatClub: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 2,
+    minMax: [1, 8]
+  },
+  handAxe: {
+    abilities: ['strength'],
+    classification: ['melee', 'ranged'],
+    cost: 5,
+    minMax: [1, 6]
+  },
+  javelin: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 1,
+    minMax: [1, 6]
+  },
+  lightHammer: {
+    abilities: ['strength'],
+    classification: ['melee', 'ranged'],
+    cost: 2,
+    minMax: [1, 4]
+  },
+  mace: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 5,
+    minMax: [1, 6]
+  },
+  quarterstaff: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 2,
+    minMax: [1, 6]
+  },
+  sickle: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 1,
+    minMax: [1, 4]
+  },
+  sickle: {
+    abilities: ['strength'],
+    classification: ['melee', 'ranged'],
+    cost: 1,
+    minMax: [1, 6]
+  },
+  lightCrossbow: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 25,
+    hasAmmunition: true,
+    minMax: [1, 6]
+  },
+  shortBow: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 25,
+    hasAmmunition: true,
+    minMax: [1, 6]
+  },
+  sling: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 1,
+    hasAmmunition: true,
+    minMax: [1, 4]
+  },
+  battleaxe: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 10,
+    minMax: [1, 8]
+  },
+  flail: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 10,
+    minMax: [1, 8]
+  },
+  greatAxe: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 30,
+    minMax: [1, 12]
+  },
+  greatSword: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 50,
+    minMax: [2, 12]
+  },
+  halberd: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 20,
+    minMax: [1, 10]
+  },
+  lance: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 10,
+    minMax: [1, 12]
+  },
+  longsword: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 15,
+    minMax: [1, 8]
+  },
+  rapier: {
+    abilities: ['dexterity', 'strength'],
+    classification: ['melee'],
+    cost: 25,
+    minMax: [1, 8]
+  },
+  shortSword: {
+    abilities: ['dexterity', 'strength'],
+    classification: ['melee'],
+    cost: 10,
+    minMax: [1, 6]
+  },
+  trident: {
+    abilities: ['strength'],
+    classification: ['melee', 'ranged'],
+    cost: 5,
+    minMax: [1, 6]
+  },
+  warHammer: {
+    abilities: ['strength'],
+    classification: ['melee'],
+    cost: 15,
+    minMax: [1, 8]
+  },
+  heavyCrossbow: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 50,
+    hasAmmunition: true,
+    minMax: [1, 10]
+  },
+  longBow: {
+    abilities: ['strength'],
+    classification: ['ranged'],
+    cost: 50,
+    hasAmmunition: true,
+    minMax: [1, 8]
+  }
+};
