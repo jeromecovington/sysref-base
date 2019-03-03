970379061285154f5d7b845fa59a24c478d4a640
Jerome Covington <jeromecovington@gmail.com>
2019-03-03 15:41:14 -0500

    add provider

---
+++ src/bestiary/helpers.js
@@ -0,0 +1,38 @@
+const BaseCreature = require('../creature/BaseCreature')
+
+module.exports = function provider ({
+  strength,
+  dexterity,
+  constitution,
+  intelligence,
+  wisdom,
+  charisma,
+  advantages,
+  disadvantages,
+  hitPointsRollFunc,
+  hitPointsModifier = 0,
+  armor,
+  weapon,
+  treasure,
+  name
+}) {
+  const creature = new BaseCreature({
+    strength,
+    dexterity,
+    constitution,
+    intelligence,
+    wisdom,
+    charisma,
+    advantages,
+    disadvantages,
+    hitPointsRollFunc
+  })
+
+  creature.setHitPoints(hitPointsModifier)
+  creature.setInventory({ armor, weapon, treasure })
+  creature.setArmorEquipped(armor)
+  creature.setWeaponEquipped(weapon)
+  creature.name = name
+
+  return creature
+}
