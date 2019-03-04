bab9aaf10e77c778e5a95aa9468e7dceacae75e5
Jerome Covington <jeromecovington@gmail.com>
2019-03-03 22:08:01 -0500

    add goblin

---
+++ src/bestiary/Goblin.js
@@ -0,0 +1,19 @@
+const { provider } = require('./helper')
+const { roll } = require('../roll/helper')
+
+module.exports = provider({
+  strength: 8,
+  dexterity: 14,
+  constitution: 10,
+  intelligence: 10,
+  wisdom: 8,
+  charisma: 8,
+  advantages: [],
+  disadvantages: [],
+  hitPointsRollFunc: () => roll(2, 12),
+  hitPointsModifier: 0,
+  armor: 'leather',
+  weapon: 'shortSword',
+  treasure: 50,
+  name: 'Goblin'
+})
