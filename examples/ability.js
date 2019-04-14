14f260d0a330aa0741b52ca4eca2ab7719190ff8
Jerome T.K. Covington <jeromecovington@gmail.com>
2019-04-14 11:38:00 -0400

    refactor ability check example

--- examples/ability.js
+++ examples/ability.js
@@ -1,8 +1,9 @@
-const { BaseCreature, BaseRoll, roll } = require('../index')
+const { BaseRoll, roll } = require('../index')
+const BaseCharacter = require('../src/character/BaseCharacter')
 
 const Roll = new BaseRoll()
 
-const Theif = new BaseCreature({
+const Rogue = new BaseCharacter({
   strength: 8,
   dexterity: 16,
   constitution: 10,
@@ -11,13 +12,18 @@ const Theif = new BaseCreature({
   charisma: 12,
   advantages: ['dexterity'],
   disadvantages: ['strength'],
-  hitPointsRollFunc: () => roll(1, 10)
+  hitPointsRollFunc: () => roll(1, 10),
+  type: 'Rogue',
+  level: 10,
+  name: 'Randolf'
 })
 
-console.log('The theif attempts to sneak past the sleeping giant.')
-const result = Roll.abilityCheck(Theif, 'stealth', 'medium')
+const moniker = `${Rogue.getName()} the ${Rogue.getType()}`
+
+console.log(`${moniker} attempts to sneak past the sleeping giant.`)
+const result = Roll.abilityCheck(Rogue, 'stealth', 'medium')
 if (result) {
-  console.log('The theif enters the giant\'s treasure room undetected.')
+  console.log(`${moniker} enters the giant's treasure room undetected.`)
 } else {
   console.log('The giant awakens, and roars angrily.')
 }
