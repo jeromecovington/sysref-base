1c516ab4754f9a5bb1b06c8fb66529157502447f
Jerome T.K. Covington <jeromecovington@gmail.com>
2020-12-27 21:39:22 -0500

    feat: implement initiative

--- src/roll/abilityMap.js
+++ src/roll/abilityMap.js
@@ -3,7 +3,7 @@ module.exports = {
   charisma: ['deception', 'intimidation', 'performance', 'persuasion'],
   constitution: [], // Constitution ability checks are rare, its modifier
   // primarily applies to hit points.
-  dexterity: ['acrobatics', 'sleight-of-hand', 'stealth'],
+  dexterity: ['acrobatics', 'sleight-of-hand', 'stealth', 'initiative'],
   intelligence: ['arcana', 'history', 'investigation', 'nature', 'religion'],
   strength: ['athletics'],
   wisdom: ['animal-handling', 'insight', 'medicine', 'perception', 'survival']
